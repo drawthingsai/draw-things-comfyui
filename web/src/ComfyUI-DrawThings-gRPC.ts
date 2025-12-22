baa9170ea47345754360953fd5871b44cd4015e0
Kelly Jerrell <kcjerrell@gmail.com>
2025-12-22 10:15:51 -0700

    1.9.0

--- web/src/ComfyUI-DrawThings-gRPC.ts
+++ web/src/ComfyUI-DrawThings-gRPC.ts
@@ -8,7 +8,7 @@ import { DtModelTypeHandler } from "./models.js";
 import { checkVersion } from "./upgrade.js";
 import { setCallback, updateProto } from "./util.js";
 
-export const nodePackVersion = "1.8.3";
+export const nodePackVersion = "1.9.0";
 
 export default {
     name: "core",
